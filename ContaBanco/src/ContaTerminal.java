e37c61d0aec7c9bb17f2ab79e52c618569116af5
Pedro Henrique <pedrosales2806@gmail.com>
2024-05-24 21:41:15 -0300

    Adição de projeto de conta bancaria.

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Informe seu nome: ");
+        String nome = scan.nextLine();
+        System.out.print("Informe sua agencia: ");
+        String agencia = scan.nextLine();
+        System.out.print("Informe o número: ");
+        int numero = scan.nextInt();
+        System.out.print("Informe seu saldo: ");
+        double saldo = scan.nextDouble();
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia + ", conta " + numero + " e seu saldo R$" + saldo + " já está disponível para saque");
+    }
+}
